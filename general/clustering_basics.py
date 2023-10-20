6aab2a5d4778c961f9b2b93726bcf057b7ce72ad
David <avina.david.s@gmail.com>
2023-10-20 13:06:44 -0700

    Clustering methods based on the elbow method

--- general/clustering_basics.py
+++ general/clustering_basics.py
@@ -1,22 +1,15 @@
 import numpy as np
+import csv
+import matplotlib.pyplot as plt
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans
-from sklearn.metrics import silhouette_score
 
-# Sample vendor names (you can replace this list with your data)
-vendor_names = [
-    "Apple Inc.",
-    "Apple",
-    "Apple Co.",
-    "Microsoft Corp.",
-    "Microsoft Inc.",
-    "Microsoft",
-    "Samsung Electronics",
-    "Samsung",
-    "Samsung Corp.",
-    "Sony Corporation",
-    "Sony"
-]
+# Read vendor names from a CSV file
+vendor_names = []
+with open('vendor_names.csv', 'r') as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        vendor_names.append(row['VendorName'])
 
 # 1. Preprocess vendor names: Convert to lowercase and strip whitespaces
 preprocessed_names = [name.lower().strip() for name in vendor_names]
@@ -25,9 +18,26 @@ preprocessed_names = [name.lower().strip() for name in vendor_names]
 vectorizer = TfidfVectorizer()
 X = vectorizer.fit_transform(preprocessed_names)
 
-# 3. Cluster the vendor names
-# For this example, let's assume we want 4 clusters (you can change this number)
-n_clusters = 4
+# Find optimal number of clusters using the elbow method
+inertias = []
+max_clusters = 10  # you can change this if you expect more clusters
+for i in range(1, max_clusters+1):
+    kmeans = KMeans(n_clusters=i, random_state=42)
+    kmeans.fit(X)
+    inertias.append(kmeans.inertia_)
+
+# Plot the inertia
+plt.figure()
+plt.plot(range(1, max_clusters+1), inertias, marker='o')
+plt.xlabel('Number of clusters')
+plt.ylabel('Inertia')
+plt.title('Elbow Method For Optimal Number of Clusters')
+plt.show()
+
+# You'll need to visually inspect the plot to determine the elbow.
+# Once you've determined the optimal number of clusters, you can run the KMeans clustering with that number:
+
+n_clusters = int(input("Enter the optimal number of clusters (based on the elbow plot): "))
 kmeans = KMeans(n_clusters=n_clusters, random_state=42)
 clusters = kmeans.fit_predict(X)
 
@@ -38,7 +48,3 @@ for i in range(n_clusters):
         if label == i:
             print(f"  {vendor_names[idx]}")
     print("")
-
-# Optionally, if you'd like to evaluate the clustering
-sil_score = silhouette_score(X, clusters)
-print(f"Silhouette Score: {sil_score:.2f}")
