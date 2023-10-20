3caf9abb91882f3052a7f8e6a096fb678d5decb3
David <avina.david.s@gmail.com>
2023-10-20 12:10:18 -0700

    Modifying the distance calculation

--- general/clustering_basics.py
+++ general/clustering_basics.py
@@ -1,6 +1,7 @@
 import nltk
 from nltk.util import ngrams
 from scipy.cluster.hierarchy import linkage, dendrogram, fcluster
+from scipy.spatial.distance import pdist, squareform
 import matplotlib.pyplot as plt
 
 nltk.download('punkt')
@@ -11,22 +12,10 @@ def jaccard_distance(str1, str2, n=3):
     s2 = set(ngrams(str2, n))
     return 1 - float(len(s1 & s2)) / float(len(s1 | s2))
 
-def create_distance_matrix(strings, n=3):
-    """Generate a matrix of Jaccard distances between each pair of strings."""
-    num_strings = len(strings)
-    distance_matrix = np.zeros((num_strings, num_strings))
-    
-    for i in range(num_strings):
-        for j in range(num_strings):
-            distance_matrix[i][j] = jaccard_distance(strings[i], strings[j], n)
-    
-    return distance_matrix
-
 def cluster_strings(strings, t=0.6, n=3):
     """Cluster strings using hierarchical clustering and Jaccard distance."""
-    distance_matrix = create_distance_matrix(strings, n)
-    
-    linkage_matrix = linkage(distance_matrix, method="single")
+    distance_array = pdist(strings, lambda x, y: jaccard_distance(x, y, n))
+    linkage_matrix = linkage(distance_array, method="single")
     
     plt.figure(figsize=(10, 5))
     dendrogram(linkage_matrix, labels=strings)
